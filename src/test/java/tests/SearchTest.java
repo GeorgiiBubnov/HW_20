00b11594eaec9cb60b45500aebcf162ac96323b5
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2024-08-11 19:23:05 +0300

    Fix Tests

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -4,21 +4,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.AppiumBy.id;
+import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
 public class SearchTest extends TestBase {
     @Test
+    @DisplayName("Проверка наличия статей")
     void successfulSearchTest() {
-        step("Type search", () -> {
+        step("Поиск статей по заданному запросу", () -> {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
-        step("Verify content found", () ->
+        step("Проверка, что перечень статей не пустой", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
@@ -39,14 +39,27 @@ public class SearchTest extends TestBase {
     }
 
 
-        @Test
-        @DisplayName("Проверка наличия заголовка")
-        void checkNewsHeaderTest () {
-            step("Проверка наличия заголовка в новостях", () -> {
-                $(id("org.wikipedia.alpha:id/view_card_header_title")).shouldHave(text("In the news"));
-            });
-        }
-
-
+    @Test
+    @DisplayName("Проверка наличия заголовка")
+    void checkNewsHeaderTest() {
+        step("Проверка наличия заголовка в новостях", () -> {
+            $(id("org.wikipedia.alpha:id/view_card_header_title")).shouldHave(text("In the news"));
+        });
     }
+
+    @Test
+    @DisplayName("Открыть найденную статью")
+    void successfulOpenArticleTest() {
+        step("Вводим поиск слова", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Открываем найденную статью", () -> $(id("page_list_item_title")).click());
+        step("Проверяем статью", () -> {
+            $(className("android.widget.TextView")).shouldHave(text("An error occurred"));
+        });
+    }
+
+
+}
 
